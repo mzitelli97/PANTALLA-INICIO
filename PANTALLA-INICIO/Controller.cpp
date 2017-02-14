1925ac4911587c084b682e1fe8070f529946f1b0
diego <diego@diego-UbuntuVirtualBox>
2017-02-14 17:21:56 -0300

    Merge origin/master
    Conflicts:
    	PANTALLA-INICIO/CModel.cpp

--- PANTALLA-INICIO/Controller.cpp
+++ PANTALLA-INICIO/Controller.cpp
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Controller.cpp
- * Author: javier
- * 
- * Created on January 10, 2017, 12:41 AM
- */
-
 #include "Controller.h"
 
 Controller::Controller() {
